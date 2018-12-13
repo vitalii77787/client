2629850f7a58ee2530a64e0b91fa9d14e272e088
vitalii77787 <vitalii77787@gmail.com>
2018-12-13 23:12:26 +0200

    add sorting animation

--- src/_components/ProductList.js
+++ src/_components/ProductList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { ProductViewContainer } from './ProductView';
 import '../Style/ProductInfo/products.css'
+import Masonry from 'react-masonry-component';
 
 
 export const ProductList = ({ products }) => {
@@ -11,8 +12,13 @@ export const ProductList = ({ products }) => {
     )
 
     return (
-        <div style={{ padding: 0 }} className="product-wrapper">
-            {productElements}
-        </div>
+        <Masonry
+        className={'product-wrapper'} // default ''
+        elementType={'div'} // default 'div'
+        disableImagesLoaded={false} // default false
+        updateOnEachImageLoad={false} // default false and works only if disableImagesLoaded is false
+    > 
+      {productElements}
+         </Masonry>
     )
 }
